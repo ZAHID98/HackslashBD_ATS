8d19bcc437333fb544529837b9173c05d5945a19
ZAHID98 <hasanjahidcse98@gmail.com>
2016-12-10 19:04:29 +0600

    Organization add on Core

---
+++ ATS_hackSlash/ATS.Core/Organization.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS.Core
+{
+ public class Organization
+    {
+     public int  Id { get; set; }
+     public String  Name { get; set; }
+     public string  Code { get; set; }
+     public string Location  { get; set; }
+    }
+}
